f603025bdbf191fbc94a316a39b403d621c58308
adamordal <adam.ordal@anm.com>
2024-03-07 16:02:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
     
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.99"
+      version = "~> 2.87.0"
       configuration_aliases = [
         azurerm.vhub
       ]
